379bf571a0dc3d1a2404fb6ecbcda0d78279d0dc
tianlef <1095012807@qq.com>
2024-07-17 21:21:44 +0800

    yolo world

--- models/PaddleMIX/CE/paddlemix/yolo-world/start.sh
+++ models/PaddleMIX/CE/paddlemix/yolo-world/start.sh
@@ -27,13 +27,13 @@ export FLAGS_allocator_strategy=auto_growth
 
 echo "*******paddlemix yolo-world infer***********"
 (python infer.py \
-    --config ./YOLO-World/configs \
-    -o ./YOLO-World/pretrain/yolo_world_v2_x_obj365v1_goldg_cc3mlite_pretrain-8698fbfa.pdparams \
-    --image https://bj.bcebos.com/v1/paddlenlp/models/community/GroundingDino/000000004505.jpg \
-    --text bus\
-    --topk 3 \
-    --threshold 0.6 \
-    --output_dir ./yolo_output) 2>&1 | tee ${log_dir}/paddlemix_yolo-world_infer.log
+    --config=./YOLO-World/configs \
+    -o=./YOLO-World/pretrain/yolo_world_v2_x_obj365v1_goldg_cc3mlite_pretrain-8698fbfa.pdparams \
+    --image=https://bj.bcebos.com/v1/paddlenlp/models/community/GroundingDino/000000004505.jpg \
+    --text=bus\
+    --topk=3 \
+    --threshold=0.6 \
+    --output_dir=./yolo_output) 2>&1 | tee ${log_dir}/paddlemix_yolo-world_infer.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
