6c123fc731e95b2ee573d708c295f83771265f7f
tianlef <1095012807@qq.com>
2024-07-22 20:25:10 +0800

    paddlemix deploy

--- models/PaddleMIX/CE/paddlemix/deploy/start.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/start.sh
@@ -10,9 +10,9 @@ exit_code=0
 # python -m pip install https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-TagBuild-Training-Linux-Gpu-Cuda11.7-Cudnn8-Mkl-Avx-Gcc8.2/latest/paddlepaddle_gpu-0.0.0.post117-cp310-cp310-linux_x86_64.whl
 # unset http_proxy
 # unset https_proxy
-
-bash blip2_deploy.sh
-exit_code=$(($exit_code + $?))
+# 即将下线
+# bash blip2_deploy.sh
+# exit_code=$(($exit_code + $?))
 
 # python -m pip install paddlepaddle-gpu==2.5.1.post117 -f https://www.paddlepaddle.org.cn/whl/linux/mkl/avx/stable.html -i http://pip.baidu.com/root/baidu/+simple/ --trusted-host pip.baidu.com
 
