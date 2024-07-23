7d7a6ccc70345aef21dd8635e1dfcb39c74dea82
tianlef <1095012807@qq.com>
2024-07-23 15:58:30 +0800

    deploy mix qwen

--- models/PaddleMIX/CE/paddlemix/deploy/qwen_export_language.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/qwen_export_language.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
-export PYTHONPATH=../../PaddleNLP/:../../../PaddleMIX:../../PaddleNLP/llm
+
 
 cd PaddleNLP/llm
-
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=../../PaddleNLP/:../../../PaddleMIX:../../PaddleNLP/llm
+export FLAGS_use_cuda_managed_memory=true
+export FLAGS_allocator_strategy=auto_growth
+export FLAGS_embedding_deterministic=1
+export FLAGS_cudnn_deterministic=1
 python predict/export_model.py \
     --model_name_or_path "qwen-vl/qwen-vl-7b-static" \
     --output_path ./checkpoints/encode_text/ \
@@ -12,6 +16,9 @@ python predict/export_model.py \
     --inference_model \
     --model_prefix qwen \
     --model_type qwen-img2txt
-
+unset FLAGS_use_cuda_managed_memory
+unset FLAGS_allocator_strategy
+unset FLAGS_embedding_deterministic
+unset FLAGS_cudnn_deterministic
 cd ..
 cd ..
