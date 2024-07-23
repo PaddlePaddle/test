57eb56daa4f1f48a96950505b9e3b92c324da0fd
tianlef <1095012807@qq.com>
2024-07-23 11:09:58 +0800

    paddlemix deploy

--- models/PaddleMIX/CE/paddlemix/deploy/prepare_qwen.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/prepare_qwen.sh
@@ -1,5 +1,5 @@
-rm -rf PaddleNLP
-git clone https://github.com/PaddlePaddle/PaddleNLP.git
+wget https://codeload.github.com/PaddlePaddle/PaddleNLP/zip/refs/tags/v2.7.2
+tar xf v2.7.2
 cd PaddleNLP
 pip install -e .
 cd csrc
