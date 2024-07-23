898ae5d80ba047723dcefb068b266edeaa825464
tianlef <1095012807@qq.com>
2024-07-23 20:36:40 +0800

    ppdiffusers deploy

--- models/PaddleMIX/CE/ppdiffusers/deploy/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/deploy/prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-pip uninstall -y paddlenlp
-wget http://10.99.15.135:8000/paddlenlp-2.6.2-py3-none-any.whl
-pip install paddlenlp-2.6.2-py3-none-any.whl
-rm -rf paddlenlp-2.6.2-py3-none-any.whl
+# pip uninstall -y paddlenlp
+# wget http://10.99.15.135:8000/paddlenlp-2.6.2-py3-none-any.whl
+# pip install paddlenlp-2.6.2-py3-none-any.whl
+# rm -rf paddlenlp-2.6.2-py3-none-any.whl
+pip install paddlenlp==2.7.2
 
 work_path=${root_path}/PaddleMIX/ppdiffusers/
 echo ${work_path}/
