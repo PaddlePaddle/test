73e63191236e2028b4deb2a6345132e4d1f887e6
tianlef <1095012807@qq.com>
2024-07-24 17:01:53 +0800

    mix deploy test

--- models/PaddleMIX/CE/paddlemix/deploy/prepare_qwen.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/prepare_qwen.sh
@@ -3,4 +3,4 @@ git clone https://github.com/PaddlePaddle/PaddleNLP.git
 cd PaddleNLP
 pip install -e .
 cd csrc
-python setup_cuda.py install https://github.com/PaddlePaddle/PaddleNLP/blob/develop/csrc/README.md
+python setup_cuda.py install
