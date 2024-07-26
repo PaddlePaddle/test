b26b9ae9e44685094f97ed820e903b0a6a94a697
tianlef <1095012807@qq.com>
2024-07-26 12:53:16 +0800

    deploy llava benchamrk

--- models/PaddleMIX/CE/paddlemix/deploy/llava_predict.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/llava_predict.sh
@@ -4,4 +4,5 @@ python deploy/llava/run_static_predict.py --model_name_or_path "paddlemix/llava/
 --image_file "https://bj.bcebos.com/v1/paddlenlp/models/community/GroundingDino/000000004505.jpg" \
 --first_model_path "llava_static/encode_image/clip"  \
 --second_model_path "llava_static/encode_text/llama" \
---fp16
+--fp16 \
+--benchmark
