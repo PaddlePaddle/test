35bbf02ce4b200581eabdaa4aa6cb35096a00b09
tianlef <1095012807@qq.com>
2024-08-21 17:31:02 +0800

    test ppdiffusers infernece paddlenlp 3.0

--- models/PaddleMIX/CE/ppdiffusers/infer/start_infer.sh
+++ models/PaddleMIX/CE/ppdiffusers/infer/start_infer.sh
@@ -21,8 +21,9 @@ fi
 cd ${work_path2}
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install paddlenlp==3.0.0b0
 pip install -e .
+pip install paddlenlp==3.0.0b0
+
 pip install pytest safetensors ftfy fastcore opencv-python einops parameterized requests-mock
 pip install ligo-segments
 
