40ce82faedd4fcdc99d3925969becb6af184f2f0
tianlef <1095012807@qq.com>
2024-08-21 14:18:16 +0800

    interlm_xcomposer2

--- models/PaddleMIX/CE/paddlemix/InternLM-XComposer2/start.sh
+++ models/PaddleMIX/CE/paddlemix/InternLM-XComposer2/start.sh
@@ -28,21 +28,34 @@ export FLAGS_use_cuda_managed_memory=true
 export FLAGS_allocator_strategy=auto_growth
 export FLAGS_embedding_deterministic=1
 export FLAGS_cudnn_deterministic=1
-echo "*******paddlemix internlm_xcomposer2 single_infer***********"
-(python paddlemix/examples/internlm_xcomposer2/chat_demo.py \
-    --model_name_or_path "internlm/internlm-xcomposer2-7b" \
-    --image_path "./000000004505.jpg" \
-    --text "Please describe this image in detail.") 2>&1 | tee ${log_dir}/paddlemix_internlm_xcomposer2_single_infer.log
+# echo "*******paddlemix internlm_xcomposer2 single_infer***********"
+# (python paddlemix/examples/internlm_xcomposer2/chat_demo.py \
+#     --model_name_or_path "internlm/internlm-xcomposer2-7b" \
+#     --image_path "./000000004505.jpg" \
+#     --text "Please describe this image in detail.") 2>&1 | tee ${log_dir}/paddlemix_internlm_xcomposer2_single_infer.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "paddlemix internlm_xcomposer2 single_infer run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "paddlemix internlm_xcomposer2 single_infer run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "*******paddlemix internlm_xcomposer2 single_infer end***********"
+export FLAGS_use_cuda_managed_memory=true
+export FLAGS_allocator_strategy=auto_growth
+export FLAGS_embedding_deterministic=1
+export FLAGS_cudnn_deterministic=1
+echo "*******paddlemix internlm_xcomposer2 train bf16 O2***********"
+(python paddlemix/tools/supervised_finetune.py interlm_xcomposer2_sft_argument_bf16_O2.json) 2>&1 | tee ${log_dir}/paddlemix_internlm_xcomposer2_train_bf16_O2.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "paddlemix internlm_xcomposer2 single_infer run success" >>"${log_dir}/ce_res.log"
+    echo "paddlemix internlm_xcomposer2 train bf16 O2 run success" >>"${log_dir}/ce_res.log"
 else
-    echo "paddlemix internlm_xcomposer2 single_infer run fail" >>"${log_dir}/ce_res.log"
+    echo "paddlemix internlm_xcomposer2 train bf16 O2 run fail" >>"${log_dir}/ce_res.log"
 fi
-echo "*******paddlemix internlm_xcomposer2 single_infer end***********"
-unset FLAGS_use_cuda_managed_memory
-unset FLAGS_allocator_strategy
+echo "*******paddlemix internlm_xcomposer2 train bf16 O2 end***********"
+
 export FLAGS_use_cuda_managed_memory=true
 export FLAGS_allocator_strategy=auto_growth
 export FLAGS_embedding_deterministic=1
@@ -60,19 +73,6 @@ echo "*******paddlemix internlm_xcomposer2 train fp32 end***********"
 echo exit_code:${exit_code}
 exit ${exit_code}
 
-export FLAGS_use_cuda_managed_memory=true
-export FLAGS_allocator_strategy=auto_growth
-export FLAGS_embedding_deterministic=1
-export FLAGS_cudnn_deterministic=1
-echo "*******paddlemix internlm_xcomposer2 train bf16 O2***********"
-(python paddlemix/tools/supervised_finetune.py interlm_xcomposer2_sft_argument.json) 2>&1 | tee ${log_dir}/paddlemix_internlm_xcomposer2_train_bf16_O2.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "paddlemix internlm_xcomposer2 train bf16 O2 run success" >>"${log_dir}/ce_res.log"
-else
-    echo "paddlemix internlm_xcomposer2 train bf16 O2 run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******paddlemix internlm_xcomposer2 train bf16 O2 end***********"
+
 echo exit_code:${exit_code}
 exit ${exit_code}
