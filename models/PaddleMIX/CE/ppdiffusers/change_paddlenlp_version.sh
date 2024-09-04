d6e7cebe11b9443fc55a83a4932b467798e45293
tianlef <1095012807@qq.com>
2024-09-04 19:20:54 +0800

    paddletest-110 change paddlenlp 2 develop

--- models/PaddleMIX/CE/ppdiffusers/change_paddlenlp_version.sh
+++ models/PaddleMIX/CE/ppdiffusers/change_paddlenlp_version.sh
@@ -4,13 +4,13 @@
 
 #pip install paddlenlp==3.0.0b0
 
-pip install paddlenlp==3.0.0b1
+#pip install paddlenlp==3.0.0b1
 
 # 尝试切换到develop的部分
 
 
-# nlp_path=${root_path}/PaddleMIX/PaddleNLP/
-# pushd ${nlp_path} || exit
-# pip install -e .
-# popd
+nlp_path=${root_path}/PaddleMIX/PaddleNLP/
+pushd ${nlp_path} || exit
+pip install -e .
+popd
 
