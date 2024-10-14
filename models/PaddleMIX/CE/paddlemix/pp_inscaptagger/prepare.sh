30c821e98f59e3120f6a37384d2dfbb57b45b721
tianlef <1095012807@qq.com>
2024-10-14 20:00:03 +0800

    pp_inscaptagger

---
+++ models/PaddleMIX/CE/paddlemix/pp_inscaptagger/prepare.sh
@@ -0,0 +1,17 @@
+cp ../change_paddlenlp_version.sh ${work_path}
+
+pip install -r requirements.txt
+pip install -e .
+
+cd ppdiffusers
+pip install -r requirements.txt
+pip install -e .
+
+cd ..
+
+bash change_paddlenlp_version.sh
+
+
+
+
+
