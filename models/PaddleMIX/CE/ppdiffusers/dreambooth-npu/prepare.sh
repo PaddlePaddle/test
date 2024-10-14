d3e0e1a4968c16d89927169b65b5bdd120ed82dc
tianlef <1095012807@qq.com>
2024-10-14 16:59:36 +0800

    paddlemix npu

---
+++ models/PaddleMIX/CE/ppdiffusers/dreambooth-npu/prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ${root_path}/ppdiffusers/
+pip install -e .
+
+pip install visualdl
+pip install -r requirements_sd3.txt
+bash ${root_path}/PaddleMIX/change_paddlenlp_version.sh
+
+
+wget https://paddlenlp.bj.bcebos.com/models/community/westfish/develop-sdxl/dog.zip
+unzip dog.zip
+rm -rf dog.zip
