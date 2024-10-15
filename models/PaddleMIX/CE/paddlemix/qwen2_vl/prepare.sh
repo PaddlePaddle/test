53a9cab9ebf2df5beeec9c5f5b0b9ba44773b768
tianlef <1095012807@qq.com>
2024-10-15 16:07:18 +0800

    qwen2_vl

--- models/PaddleMIX/CE/paddlemix/qwen2_vl/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/qwen2_vl/prepare.sh
@@ -1,4 +1,4 @@
-mix_path=${root_path}/PaddeMIX
+mix_path=${root_path}/PaddleMIX
 cd ${mix_path}
 rm -rf paddlepaddle_gpu-0.0.0-cp310-cp310-linux_x86_64.whl
 wget install https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-TagBuild-Training-Linux-Gpu-Cuda11.8-Cudnn8.6-Mkl-Avx-Gcc8.2-SelfBuiltPypiUse/latest/paddlepaddle_gpu-0.0.0-cp310-cp310-linux_x86_64.whl
