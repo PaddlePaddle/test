f9dc46f008f24319cecea6641b7802b596619fba
tianlef <1095012807@qq.com>
2024-10-17 02:01:44 +0800

    ppdiffusers npu

--- models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/npu_start.sh
+++ models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/npu_start.sh
@@ -88,27 +88,27 @@ export FLAGS_allocator_strategy=auto_growth
 # fi
 # echo "*******paddlemix text_to_image_sdxl_lora_train_unet end***********"
 
-echo "*******paddlemix text_to_image_sdxl_infer begin begin***********"
-(python test_lora_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_infer.log
+echo "*******paddlemix text_to_image_sdxl_lora_infer begin begin***********"
+(python test_lora_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_lora_infer.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_infer run success" >>"${log_dir}/ce_res.log"
+    echo "text_to_image_sdxl_lora_infer run success" >>"${log_dir}/ce_res.log"
 else
-    echo "text_to_image_sdxl_infer run fail" >>"${log_dir}/ce_res.log"
+    echo "text_to_image_sdxl_lora_infer run fail" >>"${log_dir}/ce_res.log"
 fi
-echo "*******paddlemix text_to_image_sdxl_infer end***********"
+echo "*******paddlemix text_to_image_sdxl_lora_infer end***********"
 
-echo "*******paddlemix text_to_image_sdxl_multi_infer begin begin***********"
-(python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_infer.log
+echo "*******paddlemix text_to_image_sdxl_multi_lora_infer begin begin***********"
+(python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_lora_infer.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_multi_infer run success" >>"${log_dir}/ce_res.log"
+    echo "text_to_image_sdxl_multi_lora_infer run success" >>"${log_dir}/ce_res.log"
 else
-    echo "text_to_image_sdxl_multi_infer run fail" >>"${log_dir}/ce_res.log"
+    echo "text_to_image_sdxl_multi_lora_infer run fail" >>"${log_dir}/ce_res.log"
 fi
-echo "*******paddlemix text_to_image_sdxl_multi_infer end***********"
+echo "*******paddlemix text_to_image_sdxl_multi_lora_infer end***********"
 
 
 unset http_proxy
