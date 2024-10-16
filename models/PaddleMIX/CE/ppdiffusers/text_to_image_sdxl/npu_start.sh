8a38c2df4cd3b5451bc5293520a9a41edd18a316
tianlef <1095012807@qq.com>
2024-10-17 00:20:50 +0800

    ppdiffusers npu

--- models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/npu_start.sh
+++ models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/npu_start.sh
@@ -19,105 +19,105 @@ exit_code=0
 
 bash prepare.sh
 
-export FLAGS_npu_storage_format=0
-export FLAGS_use_stride_kernel=0
+# export FLAGS_npu_storage_format=0
+# export FLAGS_use_stride_kernel=0
 
-cd ${work_path}
+# cd ${work_path}
 
-echo "*******paddlemix text_to_image_sdxl_train begin begin***********"
-(bash train.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_train.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_train run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_train run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_train end***********"
-
-
-echo "*******paddlemix text_to_image_sdxl_infer begin begin***********"
-(python test_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_infer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_infer run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_infer run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_infer end***********"
-
-echo "*******paddlemix text_to_image_sdxl_multi_infer begin begin***********"
-(python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_infer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_multi_infer run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_multi_infer run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_multi_infer end***********"
+# echo "*******paddlemix text_to_image_sdxl_train begin begin***********"
+# (bash train.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_train.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_train run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_train run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_train end***********"
 
 
-export FLAGS_npu_storage_format=0
-export FLAGS_use_stride_kernel=0
-export FLAGS_npu_scale_aclnn=True
-export FLAGS_allocator_strategy=auto_growth
+# echo "*******paddlemix text_to_image_sdxl_infer begin begin***********"
+# (python test_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_infer.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_infer run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_infer run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_infer end***********"
+
+# echo "*******paddlemix text_to_image_sdxl_multi_infer begin begin***********"
+# (python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_infer.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_multi_infer run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_multi_infer run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_multi_infer end***********"
 
 
-echo "*******paddlemix text_to_image_sdxl_lora_train begin begin***********"
-(bash lora_train.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_lora_train.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_lora_train run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_lora_train run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_lora_train end***********"
+# export FLAGS_npu_storage_format=0
+# export FLAGS_use_stride_kernel=0
+# export FLAGS_npu_scale_aclnn=True
+# export FLAGS_allocator_strategy=auto_growth
 
 
-echo "*******paddlemix text_to_image_sdxl_lora_train_unet begin begin***********"
-(bash lora_train_unet.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_lora_train_unet.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_lora_train_unet run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_lora_train_unet run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_lora_train_unet end***********"
-
-echo "*******paddlemix text_to_image_sdxl_infer begin begin***********"
-(python test_lora_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_infer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_infer run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_infer run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_infer end***********"
-
-echo "*******paddlemix text_to_image_sdxl_multi_infer begin begin***********"
-(python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_infer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "text_to_image_sdxl_multi_infer run success" >>"${log_dir}/ut_res.log"
-else
-    echo "text_to_image_sdxl_multi_infer run fail" >>"${log_dir}/ut_res.log"
-fi
-echo "*******paddlemix text_to_image_sdxl_multi_infer end***********"
+# echo "*******paddlemix text_to_image_sdxl_lora_train begin begin***********"
+# (bash lora_train.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_lora_train.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_lora_train run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_lora_train run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_lora_train end***********"
 
 
-unset http_proxy
-unset https_proxy
+# echo "*******paddlemix text_to_image_sdxl_lora_train_unet begin begin***********"
+# (bash lora_train_unet.sh) 2>&1 | tee ${log_dir}/text_to_image_sdxl_lora_train_unet.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_lora_train_unet run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_lora_train_unet run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_lora_train_unet end***********"
+
+# echo "*******paddlemix text_to_image_sdxl_infer begin begin***********"
+# (python test_lora_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_infer.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_infer run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_infer run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_infer end***********"
+
+# echo "*******paddlemix text_to_image_sdxl_multi_infer begin begin***********"
+# (python test_multi_checkpoint_infer.py) 2>&1 | tee ${log_dir}/text_to_image_sdxl_multi_infer.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "text_to_image_sdxl_multi_infer run success" >>"${log_dir}/ut_res.log"
+# else
+#     echo "text_to_image_sdxl_multi_infer run fail" >>"${log_dir}/ut_res.log"
+# fi
+# echo "*******paddlemix text_to_image_sdxl_multi_infer end***********"
+
+
+# unset http_proxy
+# unset https_proxy
 
 
 
-# 查看结果
-cat ${log_dir}/ut_res.log
+# # 查看结果
+# cat ${log_dir}/ut_res.log
 
-echo exit_code:${exit_code}
-exit ${exit_code}
+# echo exit_code:${exit_code}
+# exit ${exit_code}
