f43b43c54b4c0925bbf02c796c63880c5953f3c5
tianlef <1095012807@qq.com>
2024-10-18 13:20:20 +0800

    intern vl2

--- models/PaddleMIX/CE/paddlemix/InternVL2/start.sh
+++ models/PaddleMIX/CE/paddlemix/InternVL2/start.sh
@@ -14,7 +14,6 @@ fi
 
 /bin/cp -rf ../change_paddlenlp_version.sh ${work_path}
 /bin/cp -rf ./* ${work_path}
-# cp ../change_paddlenlp_version.sh ${work_path}
 
 cd ${work_path}
 exit_code=0
@@ -23,63 +22,59 @@ exit_code=0
 
 export http_proxy=${mix_proxy}
 export https_proxy=${mix_proxy}
-# rm -rf tests/pipelines/test_pipelines.py
-# rm -rf tests/pipelines/stable_diffusion/test_stable_diffusion_pix2pix_zero.py
+
 
 exit_code=0
-# export CUDA_VISIBLE_DEVICES=0
-export HF_ENDPOINT=https://hf-mirror.com
 export no_proxy=baidu.com,127.0.0.1,0.0.0.0,localhost,bcebos.com,pip.baidu-int.com,mirrors.baidubce.com,repo.baidubce.com,repo.bcm.baidubce.com,pypi.tuna.tsinghua.edu.cn,aistudio.baidu.com
-export USE_PPXFORMERS=true
 
-# #bash prepare.sh
-# # 准备图片做物料
-# echo "*******paddlemix InternVL2_picture_infer begin begin***********"
-# cp ${work_path}/paddlemix/demo_images/examples_image1.jpg .
+bash prepare.sh
+# 准备图片做物料
+echo "*******paddlemix InternVL2_picture_infer begin begin***********"
+cp ${work_path}/paddlemix/demo_images/examples_image1.jpg .
 
-# (python paddlemix/examples/internvl2/chat_demo.py \
-#     --model_name_or_path "OpenGVLab/InternVL2-8B" \
-#     --image_path 'examples_image1.jpg' \
-#     --text "Please describe this image in detail.") 2>&1 | tee ${log_dir}/InternVL2_picture_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "InternVL2_picture_infer run success" >>"${log_dir}/ut_res.log"
-# else
-#     echo "InternVL2_picture_infer run fail" >>"${log_dir}/ut_res.log"
-# fi
-# echo "*******paddlemix InternVL2_picture_infer end***********"
-
-
-# echo "*******paddlemix InternVL2_video_infer begin begin***********"
-# # 准备视频做物料
-# cp ${work_path}/paddlemix/demo_images/red-panda.mp4 .
-
-# (python paddlemix/examples/internvl2/chat_demo_video.py \
-#     --model_name_or_path "OpenGVLab/InternVL2-8B" \
-#     --video_path 'red-panda.mp4' \
-#     --text "Please describe this video in detail.") 2>&1 | tee ${log_dir}/InternVL2_video_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "InternVL2_video_infer run success" >>"${log_dir}/ut_res.log"
-# else
-#     echo "InternVL2_video_infer run fail" >>"${log_dir}/ut_res.log"
-# fi
-# echo "*******paddlemix InternVL2_video_infer end***********"
+(python paddlemix/examples/internvl2/chat_demo.py \
+    --model_name_or_path "OpenGVLab/InternVL2-8B" \
+    --image_path 'examples_image1.jpg' \
+    --text "Please describe this image in detail.") 2>&1 | tee ${log_dir}/InternVL2_picture_infer.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "InternVL2_picture_infer run success" >>"${log_dir}/ut_res.log"
+else
+    echo "InternVL2_picture_infer run fail" >>"${log_dir}/ut_res.log"
+fi
+echo "*******paddlemix InternVL2_picture_infer end***********"
 
 
-# echo "*******paddlemix InternVL2_train begin begin***********"
-# # 只测2B模型即可 32G以下显存
-# (bash train_internvl2.sh) 2>&1 | tee ${log_dir}/InternVL2_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "InternVL2_train run success" >>"${log_dir}/ut_res.log"
-# else
-#     echo "InternVL2_train run fail" >>"${log_dir}/ut_res.log"
-# fi
-# echo "*******paddlemix InternVL2_train end***********"
+echo "*******paddlemix InternVL2_video_infer begin begin***********"
+# 准备视频做物料
+cp ${work_path}/paddlemix/demo_images/red-panda.mp4 .
+
+(python paddlemix/examples/internvl2/chat_demo_video.py \
+    --model_name_or_path "OpenGVLab/InternVL2-8B" \
+    --video_path 'red-panda.mp4' \
+    --text "Please describe this video in detail.") 2>&1 | tee ${log_dir}/InternVL2_video_infer.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "InternVL2_video_infer run success" >>"${log_dir}/ut_res.log"
+else
+    echo "InternVL2_video_infer run fail" >>"${log_dir}/ut_res.log"
+fi
+echo "*******paddlemix InternVL2_video_infer end***********"
+
+
+echo "*******paddlemix InternVL2_train begin begin***********"
+# 只测2B模型即可 32G以下显存
+(bash train_internvl2.sh) 2>&1 | tee ${log_dir}/InternVL2_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "InternVL2_train run success" >>"${log_dir}/ut_res.log"
+else
+    echo "InternVL2_train run fail" >>"${log_dir}/ut_res.log"
+fi
+echo "*******paddlemix InternVL2_train end***********"
 
 echo "*******paddlemix InternVL2_after_train_infer begin begin***********"
 (python paddlemix/examples/internvl2/chat_demo.py \
