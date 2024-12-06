b66508d8af1bcf014d9e66f711930ba87dcd1114
tianlef <1095012807@qq.com>
2024-12-06 13:15:02 +0800

    paddlemix llava train

--- models/PaddleMIX/CE/paddlemix/llava/start.sh
+++ models/PaddleMIX/CE/paddlemix/llava/start.sh
@@ -38,28 +38,28 @@ export FLAGS_allocator_strategy=auto_growth
 # fi
 # echo "*******paddlemix llava infer end***********"
 
-# echo "*******paddlemix llava finetune***********"
-# # llava pretain 有报错
-# (python paddlemix/examples/llava/pretrain.py llava_v100_pretrain.json) 2>&1 | tee ${log_dir}/paddlemix_llava_finetune.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava finetune run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava finetune run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava finetune end***********"
+echo "*******paddlemix llava finetune***********"
+# llava pretain 有报错
+(python paddlemix/examples/llava/pretrain.py llava_v100_pretrain.json) 2>&1 | tee ${log_dir}/paddlemix_llava_finetune.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava finetune run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava finetune run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava finetune end***********"
 
-# echo "*******paddlemix llava sft***********"
-# (python paddlemix/tools/supervised_finetune.py llava_v100_sft.json) 2>&1 | tee ${log_dir}/paddlemix_llava_sft.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava sft run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava sft run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava sft end***********"
+echo "*******paddlemix llava sft***********"
+(python paddlemix/tools/supervised_finetune.py llava_v100_sft.json) 2>&1 | tee ${log_dir}/paddlemix_llava_sft.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava sft run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava sft run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava sft end***********"
 
 echo "*******paddlemix llava lora***********"
 
