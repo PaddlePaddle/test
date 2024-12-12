89fc525105fe8098278f881f4bc9a6659648d6a4
tianlef <1095012807@qq.com>
2024-12-12 15:40:30 +0800

    qwen2_vl

--- models/PaddleMIX/CE/paddlemix/qwen2_vl/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/qwen2_vl/prepare.sh
@@ -3,15 +3,15 @@ mix_path=${root_path}/PaddleMIX
 
 # 数据集下载
 cd ${mix_path}
+rm -rf playground
 if [ -e "playground.tar" ]; then
     tar -xf playground.tar
     echo "playground.tar文件存在，已解压"
 else
-    wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground.tar
+    wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground.tar --no-proxy
     tar -xf playground.tar
     echo "playground不存在，已下载解压"
 fi
-
 cd playground
 if [ -e "opensource_json.tar"]; then
     tar xf opensource_json.tar
