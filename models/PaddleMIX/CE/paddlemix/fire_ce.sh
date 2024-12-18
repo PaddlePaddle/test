0b0095d39d4a2ef9b63e75521576bfe4e5c1009b
tianlef <1095012807@qq.com>
2024-12-18 17:07:54 +0800

    mix ce

--- models/PaddleMIX/CE/paddlemix/fire_ce.sh
+++ models/PaddleMIX/CE/paddlemix/fire_ce.sh
@@ -28,7 +28,7 @@ run_list=("llava/", "qwen_vl/", "qwen2_vl/","InternVL2/", "InternLM-XComposer2/"
 # 将数组转换为字符串，以便使用正则匹配
 # 遍历当前目录下的子目录
 for subdir in */; do
-  if [[ " ${run_list[@]} " =~ " $subdir " ]]; then
+  if printf '%s\n' "${run_list[@]}" | grep -qx "$subdir"; then
     echo "子目录 ${subdir} 在列表中"
     start_script_path="$subdir/start.sh"
     # 检查start.sh文件是否存在
