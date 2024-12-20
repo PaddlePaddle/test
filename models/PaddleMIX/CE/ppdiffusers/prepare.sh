9b620d7a84c6d1b078045057c388f025d4a177ff
tianlef <1095012807@qq.com>
2024-12-20 11:33:38 +0800

    ppdiffusers models

--- models/PaddleMIX/CE/ppdiffusers/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/prepare.sh
@@ -5,14 +5,9 @@ work_path2=${root_path}/PaddleMIX/ppdiffusers/
 echo ${work_path2}/
 
 cd ${work_path2}
-# export http_proxy=${proxy}
-# export https_proxy=${proxy}
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 pip install -e .
 
-bash ${root_path}/PaddleMIX/change_paddlenlp_version.sh
 
-
-# unset http_proxy
-# unset https_proxy
