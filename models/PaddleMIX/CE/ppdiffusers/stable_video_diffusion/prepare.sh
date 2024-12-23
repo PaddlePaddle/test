22830294562ba42b1f430387bf1347ed03f04281
tianlef <1095012807@qq.com>
2024-12-23 21:53:50 +0800

    reproduce

--- models/PaddleMIX/CE/ppdiffusers/stable_video_diffusion/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/stable_video_diffusion/prepare.sh
@@ -5,3 +5,4 @@ pip install huggingface_hub==0.23.0
 wget https://paddlenlp.bj.bcebos.com/models/community/westfish/lvdm_datasets/sky_timelapse_lvdm.zip && unzip sky_timelapse_lvdm.zip
 wget https://example.com/dataset.zip && unzip dataset.zip
 rm -rf dataset.zip
+rm -rf sky_timelapse_lvdm.zip
