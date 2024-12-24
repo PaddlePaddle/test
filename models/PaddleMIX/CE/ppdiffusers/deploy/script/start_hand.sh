f1ee98c3c8580da4c5f1c1a8f1122f56be21a31c
tianlef <1095012807@qq.com>
2024-12-24 20:35:53 +0800

    ppdiffusers deploy

---
+++ models/PaddleMIX/CE/ppdiffusers/deploy/script/start_hand.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cur_path=$(pwd)
+echo ${cur_path}
+
+work_path=${root_path}/PaddleMIX/ppdiffusers/deploy
+echo ${work_path}
+
+log_dir=${root_path}/deploy_log
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+/bin/cp -rf ./* ${work_path}/
+exit_code=0
+
+cd ${work_path}
+
+#!/bin/bash
+
+cur_path=$(pwd)
+echo ${cur_path}
+
+work_path=${root_path}/PaddleMIX/ppdiffusers/deploy
+echo ${work_path}
+
+log_dir=${root_path}/deploy_log
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+/bin/cp -rf ./* ${work_path}/
+exit_code=0
+
+cd ${work_path}
+
+for subdir in */; do
+  if [ -d "$subdir" ]; then
+    echo "Testing $subdir"
+    cd "$subdir"
+    cp -f ../test_*.sh .
+    bash test_paddle.sh
+    exit_code=$((exit_code + $?))
+    bash test_paddle_tensorrt.sh
+    exit_code=$((exit_code + $?))
+    cd ..
+  fi
+done
+
+
+echo exit_code:${exit_code}
+exit ${exit_code}
